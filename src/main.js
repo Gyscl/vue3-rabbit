c20a7644292b650397c78ffbffd96e1214783bf4
yangcan <1826018405@qq.com>
2023-05-20 11:58:22 +0800

    layout-渲染一级导航列表

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+ 
 import App from './App.vue'
 import router from './router'
 
@@ -12,9 +12,8 @@ import { getCategory } from './apis/testAPI'
 getCategory().then(res=>{
     console.log(res);
 })
-const app = createApp(App)
 
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
