439f14812873307ee5e66dbd308f68d9198a4b96
yangcan <1826018405@qq.com>
2023-05-20 12:13:34 +0800

    layout实现吸顶导航的交互

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,12 +1,14 @@
 <script setup>
-  import LayoutNav from './components/LayoutNav.vue'
-  import LayoutHeader from './components/LayoutHeader.vue'
-  import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+import Layoutfixed from "./components/Layoutfixed.vue";
 </script>
 
 <template>
-  <LayoutNav/>
-  <LayoutHeader/>
+  <Layoutfixed/>
+  <LayoutNav />
+  <LayoutHeader />
   <router-view></router-view>
-  <LayoutFooter/>
+  <LayoutFooter />
 </template>
