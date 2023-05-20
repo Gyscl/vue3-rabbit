26fdfc988467cc83b5ed8343e0be6b4e8d39b753
yangcan <1826018405@qq.com>
2023-05-20 14:54:11 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,7 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 品质靠谱"/>
+  <HomePanel title="人气推荐" sub-title="人气推荐 品质靠谱"/>
 </template>
