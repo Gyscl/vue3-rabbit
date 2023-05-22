84e096fc646f659cf4455a41f9f5d8dbccb1dda7
yangcan <1826018405@qq.com>
2023-05-22 13:42:58 +0800

    二级分类-路由滚动行为体验优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,49 @@
 //createRouter：创建router实例对象
 //createWebHistory:创建history模式的路由
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
-import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
-import subCategory from '@/views/SubCategory/index.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Category/index.vue";
+import subCategory from "@/views/SubCategory/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和component对应关系的位置
   routes: [
     {
-      path:'/',
-      name:'layout',
-      component:Layout,
-      children:[
+      path: "/",
+      name: "layout",
+      component: Layout,
+      children: [
         {
-          path:'',
-          name:'home',
-          component:Home
-        }, 
-        {
-          path:'/category/:id',
-          name:'category',
-          component:Category
+          path: "",
+          name: "home",
+          component: Home,
         },
         {
-          path:'/category/sub/:id',
-          name:'subCategory',
-          component:subCategory
-        }
-      ]
+          path: "/category/:id",
+          name: "category",
+          component: Category,
+        },
+        {
+          path: "/category/sub/:id",
+          name: "subCategory",
+          component: subCategory,
+        },
+      ],
     },
     {
-      path:'/login',
-      component:Login
-    }
-  ]
-})
+      path: "/login",
+      component: Login,
+    },
+  ],
+  //路由滚动行为定制
+  scrollBehavior() {
+    return {
+      top: 0,
+    };
+  },
+});
 
-export default router
+export default router;
