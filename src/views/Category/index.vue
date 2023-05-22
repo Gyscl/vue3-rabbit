87a4b881cd57de13637b6718a2cf7eeff89ec389
yangcan <1826018405@qq.com>
2023-05-22 10:34:55 +0800

    一级分类-激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from "vue";
 import { getCategoryAPI } from "@/apis/category";
 import { getBannerAPI } from "@/apis/home";
 import { useRoute } from "vue-router"; //在组件中获取路由参数
+import GoodsItem  from '@/views/Home/components/GoodsItem.vue'
 
 const categoryData = ref({});
 const route = useRoute();
@@ -41,6 +42,29 @@ onMounted(() => getBanner());
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="ref-goods"
+        v-for="item in categoryData.children"
+        :key="item.id"
+      >
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
