743d3956ffdd743e092727a85992bdcaa5a70533
yangcan <1826018405@qq.com>
2023-05-22 12:30:44 +0800

    二级分类-整体的认识

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
-
+import subCategory from '@/views/SubCategory/index.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,15 +13,23 @@ const router = createRouter({
   routes: [
     {
       path:'/',
+      name:'layout',
       component:Layout,
       children:[
         {
           path:'',
+          name:'home',
           component:Home
         }, 
         {
           path:'/category/:id',
+          name:'category',
           component:Category
+        },
+        {
+          path:'/category/sub/:id',
+          name:'subCategory',
+          component:subCategory
         }
       ]
     },
