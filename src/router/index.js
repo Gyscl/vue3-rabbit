9cc545cfa6c1799c30d44abd0b9e1b9c730d8587
yangcan <1826018405@qq.com>
2023-06-01 15:31:25 +0800

    本地购物车-列表购物车-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import subCategory from "@/views/SubCategory/index.vue";
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,11 @@ const router = createRouter({
           path:'/detail/:id',
           name:"detail",
           component:Detail
+        },
+        {
+          path:'/cartlist',
+          name:"cartList",
+          component:CartList
         }
       ],
     },
