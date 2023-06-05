331315e9138391623c3896c89c01401dc06e6948
yangcan <1826018405@qq.com>
2023-06-05 15:36:26 +0800

    结算页-提交订单

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { getCheckInfoAPI } from "@/apis/checkout";
+import { getCheckInfoAPI, createOrderAPI } from "@/apis/checkout";
+import { useRouter } from "vue-router";
 import { onMounted, ref } from "vue";
+import {useCartStore} from "@/stores/cartStore"
+const cartStore=useCartStore()
+const router = useRouter();
 const checkInfo = ref({}); //订单对象
 const curAddress = ref({}); // 地址对象
 const getCheckInfo = async () => {
@@ -16,19 +20,45 @@ const getCheckInfo = async () => {
 onMounted(() => getCheckInfo());
 
 //控制弹框打开
-const showDialog=ref(false)
+const showDialog = ref(false);
 
 //切换地址
-const activeAddress=ref({})
-const switchAddress=(item)=>{
-  activeAddress.value=item
-}
+const activeAddress = ref({});
+const switchAddress = (item) => {
+  activeAddress.value = item;
+};
+//切换地址的确认
+const confirm = () => {
+  curAddress.value = activeAddress.value;
+  showDialog.value = false;
+  activeAddress.value = {};
+};
 
-const confirm=()=>{
-  curAddress.value=activeAddress.value
-  showDialog.value=false
-  activeAddress.value={}
-}
+//创建订单
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: "",
+    goods: checkInfo.value.goods.map((item) => {
+      return {
+        skuId: item.skuId,
+        count: item.count,
+      };
+    }),
+    addressId: curAddress.value,
+  });
+  const orderId = res.result.id;
+  router.push({
+    path: "pay",
+    query: {
+      id: orderId,
+    },
+  });
+  //更新购物车
+  cartStore.updateNewList()
+};
 </script>
 
 <template>
@@ -138,29 +168,39 @@ const confirm=()=>{
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large">提交订单</el-button>
+          <el-button type="primary" size="large" @click="createOrder"
+            >提交订单</el-button
+          >
         </div>
       </div>
     </div>
   </div>
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
-  <div class="addressWrapper">
-    <div class="text item" :class="{active:activeAddress.id===item.id}" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
-      <ul>
-      <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
-      <li><span>联系方式：</span>{{ item.contact }}</li>
-      <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
-      </ul>
+    <div class="addressWrapper">
+      <div
+        class="text item"
+        :class="{ active: activeAddress.id === item.id }"
+        @click="switchAddress(item)"
+        v-for="item in checkInfo.userAddresses"
+        :key="item.id"
+      >
+        <ul>
+          <li>
+            <span>收<i />货<i />人：</span>{{ item.receiver }}
+          </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
     </div>
-  </div>
-  <template #footer>
-    <span class="dialog-footer">
-      <el-button>取消</el-button>
-      <el-button type="primary" @click="confirm">确定</el-button>
-    </span>
-  </template>
-</el-dialog>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
