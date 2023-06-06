edef6cffe06e95bab16fa5370f2a71ff1e69e45b
yangcan <1826018405@qq.com>
2023-06-06 17:10:40 +0800

    会员中心-个人信息数据渲染

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,6 +1,6 @@
 //封装所有和用户相关的接口函数
-
 import request from '@/utils/http'
+
 export const loginAPI=({account,password})=>{
     return request({
         url:'/login',
@@ -11,3 +11,12 @@ export const loginAPI=({account,password})=>{
         }
     })
 }
+
+export const getLikeListAPI = ({ limit = 4 }) => {
+    return request({
+      url:'/goods/relevant',
+      params: {
+        limit 
+      }
+    })
+  }
